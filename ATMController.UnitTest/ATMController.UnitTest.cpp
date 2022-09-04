2a497c68b8c9c1026d7f6ad9f688cc16dd13cf3b
idujin <yujin.gha@gmail.com>
2022-09-04 15:59:18 +0900

    add unit test.

---
+++ ATMController.UnitTest/ATMController.UnitTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ATMControllerUnitTest
+{
+	TEST_CLASS(ATMControllerUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
