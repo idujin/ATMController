077e6bf87398a238086d30612a4f9b65b51dc7f8
idujin <yujin.gha@gmail.com>
2022-09-04 15:58:54 +0900

    made bank/user api to interface. capsulized the APIs.

--- ATMController/ATMController.cpp
+++ ATMController/ATMController.cpp
@@ -1,60 +1,29 @@
 #include "ATMController.h"
 
+ATMController::ATMController(){
+	Initialize();
+}
+
 void ATMController::Initialize() {
 	is_identified_ = false;
 	account_ = -1;
 }
 
-bool ATMController::Process() {
-	const int card_number = GetCardNumber();
-	if (!IsValidCardNumber(card_number)) // invalid cardnumber. request the cardnumber again.
-		return false;
+bool ATMController::Process(IBankAPI* bank, IUserInterface* user) {
+	const int card_number = ReadInsertedCard(bank, user);
+	if (card_number < 0) return false; // invalid cardnumber. request the cardnumber again.
 
-	int invalid_try_num = GetCurrentInvalidTry(card_number);
-	while (invalid_try_num <= kTryLimit) {
-		if (IsValidPIN(GetPIN())) {
-			is_identified_ = true; // identified user
-			break;
-		} else {
-			invalid_try_num++;
-			UpdateInvaidTry(card_number, invalid_try_num);
-		}
-	}
-	
+	is_identified_ = IdentifyUser(bank, user, card_number);
 	if (!is_identified_) {
 		return false; // blocked account
 	}
 
-	vector<int> accounts = GetAccounts(card_number);
-	if (accounts.size() == 0){
-		return false; // account is not connected for some reason? 
-	} else if (accounts.size() == 1) {
-		account_ = accounts[0];
-	} else {
-		account_ = accounts[SelectAccount(accounts)];
+	account_ = SelectAccount(bank, user, card_number);
+	if (account_ < 0) {
+		return false;
 	}
 
-	ProcType curr_job;
-	do {
-		curr_job = SelectProcType();
-		switch (curr_job) {
-		case ProcType::kBalance:
-			int balance = GetBalance(account_);
-			ShowBalance(balance);
-			break;
-		case ProcType::kDeposit:
-			int deposit = GetDepositAmount();
-			UpdateBalance(account_, GetBalance(account_) + deposit);
-			break;
-		case ProcType::kWithDraw:
-			DoWithDraw(); // error check
-			break;
-		case ProcType::kExit:
-			break;
-		default:
-			break;
-		}
-	} while (curr_job != ProcType::kExit);
+	DoBankJob(bank, user, account_);
 	
 	Initialize();
 
@@ -63,18 +32,81 @@ bool ATMController::Process() {
 
 }
 
-bool ATMController::DoWithDraw() {
+int ATMController::ReadInsertedCard(IBankAPI* bank, IUserInterface* user) {
+	const int card_number = user->GetCardNumber();
+	if (!bank->IsValidCardNumber(card_number)) // invalid cardnumber. request the cardnumber again.
+		return -1;
+
+	return card_number;
+}
+
+bool ATMController::IdentifyUser(IBankAPI* bank, IUserInterface* user, const int card_number) {
+	int invalid_try_num = bank->GetCurrentInvalidTry(card_number);
+	while (invalid_try_num <= kTryLimit) {
+		if (bank->IsValidPIN(user->GetPIN())) {
+			return true; // identified user
+			break;
+		}
+		else {
+			invalid_try_num++;
+			bank->UpdateInvaidTry(card_number, invalid_try_num);
+		}
+	}
+	return false;
+}
+
+int ATMController::SelectAccount(IBankAPI* bank, IUserInterface* user, const int card_number) {
+	vector<int> accounts = bank->GetAccounts(card_number);
+	int account = -1;
+	if (accounts.size() == 1) {
+		account = accounts[0];
+	} else if (accounts.size() > 1) {
+		account = accounts[user->SelectAccount(accounts)];
+	} else {
+		return account; // account is not connected for some reason? 
+	}
+	return account;
+}
+
+void ATMController::DoBankJob(IBankAPI* bank, IUserInterface* user, int account) {
+	ProcType curr_job;
+	do {
+		curr_job = user->SelectProcType();
+		switch (curr_job) {
+		case ProcType::kBalance:
+			int balance = bank->GetBalance(account);
+			user->ShowBalance(balance);
+			break;
+		case ProcType::kDeposit:
+			int deposit = user->GetDepositAmount();
+			bank->UpdateBalance(account, bank->GetBalance(account) + deposit);
+			break;
+		case ProcType::kWithDraw:
+			DoWithDraw(bank, user, account); // error check
+			break;
+		case ProcType::kExit:
+			break;
+		default:
+			break;
+		}
+	} while (curr_job != ProcType::kExit);
+
+	return;
+}
+
+
+bool ATMController::DoWithDraw(IBankAPI* bank, IUserInterface* user, const int account) {
 	int balance;
 	int get_amount;
 	do {
-		balance = GetBalance(account_);
-		ShowBalance(balance);
-		get_amount = GetWithDrawalAmount();
+		balance = bank->GetBalance(account);
+		user->ShowBalance(balance);
+		get_amount = user->GetWithDrawalAmount();
 		
 	} while (get_amount > balance);// invalid request. withdrawal amount should be less than current balance
 
-	if (RequestWithDraw(get_amount)) {
-		UpdateBalance(account_, balance - get_amount);
+	if (user->RequestWithDraw(get_amount)) {
+		bank->UpdateBalance(account_, balance - get_amount);
 	}
 	else {
 		return false; // faied for some reason? 
--- ATMController/ATMController.h
+++ ATMController/ATMController.h
@@ -8,35 +8,47 @@ enum class ProcType { kBalance = 0, kDeposit, kWithDraw, kExit };
 enum class ATMResults {kInvalidCardNumber = 2, };
 
 // Bank API
-bool IsValidCardNumber(int card_number);
-bool IsValidPIN(int pin_number);
-vector<int> GetAccounts(int card_number);
-int GetBalance(int account);
-void UpdateBalance(int account, int balance);
-int GetCurrentInvalidTry(int card_number);
-void UpdateInvaidTry(int card_number, int invalid_try_num);
-
-
+class IBankAPI {
+public:
+	virtual ~IBankAPI() = 0;
+	virtual bool IsValidCardNumber(int card_number) =0;
+	virtual bool IsValidPIN(int pin_number) = 0;
+	virtual vector<int> GetAccounts(int card_number) = 0;
+	virtual int GetBalance(int account) = 0;
+	virtual void UpdateBalance(int account, int balance) = 0;
+	virtual int GetCurrentInvalidTry(int card_number) = 0;
+	virtual void UpdateInvaidTry(int card_number, int invalid_try_num) = 0;
+};
 
 // User interface
-int GetCardNumber();
-int GetPIN();
-int SelectAccount(vector<int> accounts);
-ProcType SelectProcType();
-void ShowBalance(int balance);
-int GetDepositAmount();
-int GetWithDrawalAmount();
-bool RequestWithDraw(int get_amount);
+class IUserInterface {
+public:
+	virtual ~IUserInterface() = 0;
+	virtual int GetCardNumber() = 0;
+	virtual int GetPIN() = 0;
+	virtual int SelectAccount(vector<int> accounts) = 0;
+	virtual ProcType SelectProcType() = 0;
+	virtual void ShowBalance(int balance) = 0;
+	virtual int GetDepositAmount() = 0;
+	virtual int GetWithDrawalAmount() = 0;
+	virtual bool RequestWithDraw(int get_amount) = 0;
+};
 
 
 class ATMController {
 public:
+	ATMController();
 	void Initialize();
-	bool Process();
+	bool Process(IBankAPI* bank, IUserInterface* user);
+	int ReadInsertedCard(IBankAPI* bank, IUserInterface* user);
+	bool IdentifyUser(IBankAPI* bank, IUserInterface* user, int card_number);
+	int SelectAccount(IBankAPI* bank, IUserInterface* user, int card_number);
+	void DoBankJob(IBankAPI* bank, IUserInterface* user, int account);
+
 private:
-	bool DoWithDraw();
+	bool DoWithDraw(IBankAPI* bank, IUserInterface* user, int account);
 	bool is_identified_;
 	int account_;
-	
+
 };
 
